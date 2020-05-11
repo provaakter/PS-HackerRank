67395c4db018f4c71139b00bf426c47c293421bf
Prova Akter <cseerani@gmail.com>
2020-05-12 01:37:56 +0200

    10 - Binary Numbers uploaded

---
+++ 30 Days of Code/10 - Binary Numbers/Solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0, max = 0;
+
+        while(n != 0){
+            if(n%2==1){
+                count++;
+                if(count>=max){
+                    max = count;
+                }
+            }
+            else{
+                count = 0;
+            }
+            n = n/2;
+        }
+        
+        System.out.println(max);
+        scanner.close();
+    }
+}
